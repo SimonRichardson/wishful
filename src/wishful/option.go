fbf142c67775baeb348027ff0fadf7009f6d39d1
Simon Richardson <stickupkid@gmail.com>
2013-12-19 11:26:47 +0000

    Adding Map to option structure.

--- src/wishful/option.go
+++ src/wishful/option.go
@@ -5,6 +5,9 @@ type Value interface {}
 type Option interface {
     // Methods
     Chain(func(v Value) Option) Option
+
+    // Derived
+    Map(func(v Value) Value) Option
 }
 
 type Some struct {
@@ -19,5 +22,15 @@ func (o Some) Chain(f func(x Value) Option) Option {
 }
 
 func (o None) Chain(f func(x Value) Option) Option {
+    return o
+}
+
+// Derived
+func (o Some) Map(f func(x Value) Value) Option {
+    return o.Chain(func(x Value) Option {
+        return Some{f(x)}
+    })
+}
+func (o None) Map(f func(x Value) Value) Option {
     return o
 }
--- src/wishful/option_test.go
+++ src/wishful/option_test.go
@@ -7,21 +7,63 @@ import (
     "reflect"
 )
 
-func (o Some) generate(rand *rand.Rand, size int) reflect.Value {
+func (o Some) Generate(rand *rand.Rand, size int) reflect.Value {
     return reflect.ValueOf(Some{rand.Intn(size) - size / 2})
 }
 
-func (o None) generate(rand *rand.Rand, size int) reflect.Value {
+func (o None) Generate(rand *rand.Rand, size int) reflect.Value {
     return reflect.ValueOf(None{})
 }
 
-func TestStuff(t *testing.T) {
+func TestChainWithOptionSome(t *testing.T) {
     f := func(v int) Option {
         return Some{v * 2}
     }
     g := func(v int) Option {
         return Some{v}.Chain(func (x Value) Option {
             return Some{x.(int) * 2}
+        })
+    }
+    if err := quick.CheckEqual(f, g, nil); err != nil {
+        t.Error(err)
+    }
+}
+
+func TestChainWithOptionNone(t *testing.T) {
+    f := func(v int) Option {
+        return None{}
+    }
+    g := func(v int) Option {
+        return None{}.Chain(func (x Value) Option {
+            return Some{x.(int) * 2}
+        })
+    }
+    if err := quick.CheckEqual(f, g, nil); err != nil {
+        t.Error(err)
+    }
+}
+
+func TestMapWithOptionSome(t *testing.T) {
+    f := func(v int) Option {
+        return Some{v + 1}
+    }
+    g := func(v int) Option {
+        return Some{v}.Map(func (x Value) Value {
+            return x.(int) + 1
+        })
+    }
+    if err := quick.CheckEqual(f, g, nil); err != nil {
+        t.Error(err)
+    }
+}
+
+func TestMapWithOptionNone(t *testing.T) {
+    f := func(v int) Option {
+        return None{}
+    }
+    g := func(v int) Option {
+        return None{}.Map(func (x Value) Value {
+            return x.(int) + 1
         })
     }
     if err := quick.CheckEqual(f, g, nil); err != nil {
